726e3e0bb46ce0b5c2d6f9992f0ca86218ca01e8
wucanfish <a0968886900@gmail.com>
2024-03-13 12:01:32 +0800

    week3

---
+++ week04-1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+    int a=3;
+    while(a>0)
+    {
+        printf("a¬O%d\n",a);
+        a--;//a-=1ªºÁY¼g
+
+    }
+
+}
---
+++ week04-4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a=3;
+    while(a>0)
+    {
+        printf("a¬O%d\n",a);
+        a--;
+    }
+    for (int b=3;b>0;b--){
+        printf("b¬O%d\n",b);
+    }
+}
