5c6ca51a954176c3c2c980d11b5cf35d6f1bbd12
冠 <144758398+wucanfish@users.noreply.github.com>
2024-06-05 11:56:08 +0800

    week16

---
+++ week16/week16-5a.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int a[100];
+	int n;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	for(int i=n-1;i>=0;i--){
+		if(a[i]%2==1){
+			printf("%d ",a[i]);
+		}
+	}
+}
