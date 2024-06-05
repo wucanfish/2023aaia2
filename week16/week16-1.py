12928fa3184608f9abbae89ccc12bc6db18b3adc
冠 <144758398+wucanfish@users.noreply.github.com>
2024-06-05 11:55:16 +0800

    Delete week16 directory

--- week16/week16-1.py
+++
@@ -1,7 +0,0 @@
-from collections import Counter
-words = ["bella","label","roller"]
-for i in range(3):
-  counter=Counter(words[0])
-  print(words[0],counter)
-ans=Counter(words[0])&Counter(words[1])&Counter(words[2])
-print(ans)
--- week16/week16-4a.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(){
-	int a;
-	scanf("%d",&a);
-	printf("%02d:%02d:%02d",a/3600,a%3600/60,a%60);
-}
